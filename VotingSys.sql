2171a6b578232c62b8a59c462c20afb620c3f70c
naga sharmada <nagasharmadabolla@gmail.com>
2025-05-21 22:21:50 +0530

    Formatted the file

--- VotingSys.sql
+++ VotingSys.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS voterss (
     student_id VARCHAR(50),
     book_id INT CHECK (book_id IN (1, 2, 3,4,5,6)), -- Ensuring book_id can only be 1, 2, or 3
     PRIMARY KEY (student_id, book_id) -- Composite primary key to prevent duplicate entries
-);
-SELECT * FROM LIBRARY_DETAILS;
+) ;
+SELECT * FROM LIBRARY_DETAILS ;
